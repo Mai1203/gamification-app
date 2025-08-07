415b353804bc838cedf8d114df4da2f97245e63c
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2025-08-07 18:35:31 -0500

    <style>[favicon]: Se agregó logo de pestaña

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="es" className=''>
+        <head>
+          <link rel="icon" href="/favicon.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>EdoCode</title>
+        </head>
         <body
           className={`${inter.className} antialiased`}
         >
